1a2e12f103d0914f7f989532523ba3e84c7707dc
Dane Reid <danereid@macbookpro.mynetworksettings.com>
2024-11-25 23:55:40 -0500

    Many changes. site mostly working now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,33 +4,70 @@ import { RouterOutlet } from '@angular/router';
 import { NameCheckerResponse, NameCheckerService } from './name-checker.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule],
+    standalone: true,
+    imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule],
     template: `
   <div class="title">
   <h1>PUBG Name Checker</h1>
   </div>
-  
-  <div class="search">
-    <pre>
-      <code>
-      {{nameData?.data?.username | json}}
-      </code>
-    </pre>
+  <div class="description">
+  <h2>Search for a user's name</h2>
+  <h6>Enter the user's ID to see their username</h6>
   </div>
-  <input type="text" [(ngModel)]="id" name="id">
+  <div class="search">
+      {{nameData?.data?.username}}
+      <div *ngIf="nameData?.msg === 'id_found'">
+        User found!
+      </div>
+  </div>
+  <mat-form-field appearance="fill">
+  <input matInput class="text" type="text" [(ngModel)]="id" name="id">
+  </mat-form-field>
+  
   <div class="button">
-  <button (click)="getID()">Check</button>
+  <button mat-flat-button (click)="getID()">Check ID</button>
   </div>
   
   `,
     styles: `
-  h1 {
-    color:red
-  }
+    .title{
+      text-align: center; 
+    }
+    .search{
+      text-align: center;
+      margin-left: 30%;
+      margin-right: 30%;
+      display: block;
+      code {
+        margin-left: none;
+      }
+    }
+    input {
+      text-align: center;
+    }
+    .description{
+      text-align: center;
+    }
+    mat-form-field {
+      text-align: center;
+      margin-left: 30%;
+      margin-right: 30%;
+      display: block;
+    }
+    .button {
+      text-align: center;
+      display: block;
+    }
+    button{
+      width: 200px;
+    }
   `
 })
 export class AppComponent {
@@ -41,15 +78,27 @@ export class AppComponent {
   nameData?: NameCheckerResponse;
 
   getID() {
+
+    if (this.id.trim() === '') {
+      alert("Please insert user ID!")
+    }
+
     this.nameCheckerService.getID(this.id).subscribe({  
       next: (data) => {
         this.nameData = data;
         data.status
+        console.log(
+          this.nameData?.error,
+          this.nameData?.status,
+          this.nameData?.msg
+        )
+        if (this.nameData?.msg === 'id_not_found' || this.nameData?.error) {
+          alert("Invalid user ID!");
+          }
       },
       error: (err) => {
         console.error(err);
       },
   });
-  }
-
+  } 
 }
